51a877da75f6528df7682e9c298758b1c1de5135
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-21 12:13:29 -0600

    Refactor to import src/App.js into root App.js

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import HybridApp from './src/App';
 
-export default function App() {
+export default function App(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <HybridApp />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
