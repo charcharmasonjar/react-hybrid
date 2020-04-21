0a4a9121d41a721a4e3727eb607ac9caa4c3ef00
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-21 13:25:24 -0600

    Store selected pokemon in state and pass render prop to components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Router, Switch, Route } from "./routing";
 import Home from "./Home";
 import Pokemon from "./Pokemon";
 
 export default function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
   return (
     <View style={styles.container}>
       <Router>
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/pokemon">
-            <Pokemon />
-          </Route>
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Home {...props} setSelectedPokemon={setSelectedPokemon} />
+            )}
+          ></Route>
+          <Route
+            exact
+            path="/pokemon"
+            render={props => (
+              <Pokemon {...props} selectedPokemon={selectedPokemon} />
+            )}
+          ></Route>
         </Switch>
       </Router>
     </View>
